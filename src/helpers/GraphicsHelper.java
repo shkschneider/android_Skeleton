bfbeb5f79ac338dc4d4d64095858b17ae66a6801
shkschneider <shk.schneider@gmail.com>
2013-08-08 15:09:32 +0200

    GraphicsHelper: animate with callback

--- src/helpers/GraphicsHelper.java
+++ src/helpers/GraphicsHelper.java
@@ -21,8 +21,10 @@ import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.util.TypedValue;
 import android.widget.ProgressBar;
 
@@ -34,12 +36,18 @@ public abstract class GraphicsHelper {
             if (context != null) {
                 return context.getResources().getDisplayMetrics().density;
             }
+            else {
+                LogHelper.w("Context was NULL");
+            }
             return 0F;
         }
 
         public static int height(final Context context) {
             if (context != null) {
                 return context.getResources().getDisplayMetrics().heightPixels;
+            }
+            else {
+                LogHelper.w("Context was NULL");
             }
             return 0;
         }
@@ -48,12 +56,18 @@ public abstract class GraphicsHelper {
             if (context != null) {
                 return context.getResources().getDisplayMetrics().widthPixels;
             }
+            else {
+                LogHelper.w("Context was NULL");
+            }
             return 0;
         }
 
         public static Drawable indeterminateDrawable(final Context context) {
             if (context != null) {
                 return new ProgressBar(context).getIndeterminateDrawable();
+            }
+            else {
+                LogHelper.w("Context was NULL");
             }
             return null;
         }
@@ -69,6 +83,9 @@ public abstract class GraphicsHelper {
                 matrix.postRotate(degrees);
                 return Bitmap.createBitmap(oldBitmap, 0, 0, oldBitmap.getWidth(), oldBitmap.getHeight(), matrix, true);
             }
+            else {
+                LogHelper.w("Context was NULL");
+            }
             return null;
         }
 
@@ -76,7 +93,51 @@ public abstract class GraphicsHelper {
             if (context != null) {
                 return new BitmapDrawable(context.getResources(), GraphicsHelper.Transform.rotateBitmap(context, id, degrees));
             }
+            else {
+                LogHelper.w("Context was NULL");
+            }
             return null;
+        }
+
+        public static void animate(final AnimationDrawable animationDrawable, final Callback callback, final Long time) {
+            if (animationDrawable != null) {
+                animationDrawable.start();
+
+                if (time > 0) {
+                    animateListener(animationDrawable, callback, time);
+                }
+                else {
+                    LogHelper.w("Time was negative");
+                }
+            }
+            else {
+                LogHelper.w("AnimationDrawable was NULL");
+            }
+        }
+
+        public static void animate(final AnimationDrawable animationDrawable, final Callback callback) {
+            animate(animationDrawable, callback, 100L);
+        }
+
+        private static void animateListener(final AnimationDrawable animationDrawable, final Callback callback, final Long time) {
+            if (animationDrawable != null) {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        if (animationDrawable.getCurrent() != animationDrawable.getFrame(animationDrawable.getNumberOfFrames() - 1)) {
+                            animateListener(animationDrawable, callback, time);
+                        }
+                        else if (callback != null) {
+                            callback.AnimationCallback();
+                        }
+                        else {
+                            LogHelper.d("Callback was NULL");
+                        }
+                    }
+
+                }, time);
+            }
         }
 
     }
@@ -87,25 +148,40 @@ public abstract class GraphicsHelper {
             if (context != null && dp > 0) {
                 return (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics()));
             }
+            else {
+                LogHelper.w("Context was NULL");
+            }
             return 0;
         }
 
         public static Bitmap bitmapFromDrawable(final Drawable drawable) {
-            if (drawable instanceof BitmapDrawable) {
-                return ((BitmapDrawable)drawable).getBitmap();
+            if (drawable != null) {
+                if (drawable instanceof BitmapDrawable) {
+                    return ((BitmapDrawable)drawable).getBitmap();
+                }
+
+                final Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Config.ARGB_8888);
+                final Canvas canvas = new Canvas(bitmap);
+                drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                drawable.draw(canvas);
+
+                return bitmap;
             }
-
-            final Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Config.ARGB_8888);
-            final Canvas canvas = new Canvas(bitmap);
-            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-            drawable.draw(canvas);
-
-            return bitmap;
+            else {
+                LogHelper.w("Drawable was NULL");
+            }
+            return null;
         }
 
         public static Drawable drawableFromBitmap(final Context context, final Bitmap bitmap) {
             return new BitmapDrawable(context.getResources(), bitmap);
         }
+
+    }
+
+    public static interface Callback {
+
+        public void AnimationCallback();
 
     }
 
