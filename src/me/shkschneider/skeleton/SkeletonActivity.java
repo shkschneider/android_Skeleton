11a06db3a6a072a80442588990e0215741a30a30
shkschneider <shk.schneider@gmail.com>
2013-09-17 13:25:12 +0200

    All permissions

--- src/me/shkschneider/skeleton/SkeletonActivity.java
+++ src/me/shkschneider/skeleton/SkeletonActivity.java
@@ -17,6 +17,7 @@ package me.shkschneider.skeleton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
@@ -24,13 +25,35 @@ import com.actionbarsherlock.view.MenuItem;
 public class SkeletonActivity extends SherlockActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.skeleton);
+
+        String message = "";
+        message = message.concat("Android.getApi: " + Skeleton.Android.getAccount(SkeletonActivity.this) + "\n");
+        message = message.concat("Android.getSignature: " + Skeleton.Android.getSignature(SkeletonActivity.this) + "\n");
+        message = message.concat("Android.isTablet: " + Skeleton.Android.isTablet(SkeletonActivity.this) + "\n");
+        message = message.concat("Android.getId: " + Skeleton.Android.getId(SkeletonActivity.this) + "\n");
+        message = message.concat("Android.getDeviceId: " + Skeleton.Android.getDeviceId(SkeletonActivity.this) + "\n");
+        message = message.concat("Android.getUUID: " + Skeleton.Android.getUUID(SkeletonActivity.this) + "\n");
+        message = message.concat("Android.getRandomId: " + Skeleton.Android.getRandomId() + "\n");
+        message = message.concat("Android.getDevice: " + Skeleton.Android.getDevice() + "\n");
+        message = message.concat("Android.getRelease: " + Skeleton.Android.getRelease() + "\n");
+        message = message.concat("Android.getApi: " + Skeleton.Android.getApi() + "\n");
+        message = message.concat("Android.isDebug: " + Skeleton.Android.isDebug() + "\n");
+        message = message.concat("Android.getPackage: " + Skeleton.Android.getPackage(SkeletonActivity.this) + "\n");
+        message = message.concat("Android.getName: " + Skeleton.Android.getName(SkeletonActivity.this) + "\n");
+        message = message.concat("Android.getVersionName: " + Skeleton.Android.getVersionName(SkeletonActivity.this) + "\n");
+        message = message.concat("Android.getVersionCode: " + Skeleton.Android.getVersionCode(SkeletonActivity.this) + "\n");
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        if (textView != null) {
+            textView.setText(message);
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(final MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 startActivity(new Intent(SkeletonActivity.this, SkeletonActivity.class));
