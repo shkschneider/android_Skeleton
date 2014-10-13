1facfebbc755992343c4ae868aeb9c6bb7041160
shkschneider <shk.schneider@gmail.com>
2014-10-13 14:24:07 +0200

    Executor: fixed UIThread execution

--- app/src/me/sdk/Executor.java
+++ app/src/me/sdk/Executor.java
@@ -1,12 +1,13 @@
 package me.sdk;
 
-import java.util.concurrent.Executors;
+import android.os.Handler;
+
 import java.util.concurrent.TimeUnit;
 
 public class Executor {
 
     public static void delayedRunnable(final Runnable runnable, final int amount, TimeUnit timeUnit) {
-        Executors.newScheduledThreadPool(1).schedule(runnable, amount, timeUnit);
+        new Handler().postDelayed(runnable, timeUnit.toMillis(amount));
     }
 
 }
