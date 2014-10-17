53a7cb03234a697090bf465fc0e9057f60e62e31
shkschneider <shk.schneider@gmail.com>
2014-10-17 18:17:32 +0200

    MainLayout change

--- app/src/main/java/me/shkschneider/app/fragment/MainFragment.java
+++ app/src/main/java/me/shkschneider/app/fragment/MainFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.koushikdutta.ion.Ion;
 
 import me.shkschneider.app.R;
 import me.shkschneider.skeleton.helper.ApplicationHelper;
@@ -20,10 +23,15 @@ public class MainFragment extends SkeletonFragment {
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        final TextView textView1 = (TextView) view.findViewById(android.R.id.text1);
-        textView1.setText(ApplicationHelper.name());
-        final TextView textView2 = (TextView) view.findViewById(android.R.id.text2);
-        textView2.setText(ApplicationHelper.versionName());
+        final ImageView authorAvatar = (ImageView) view.findViewById(R.id.authorAvatar);
+        Ion.with(authorAvatar)
+                .load("https://shkschneider.me/shkschneider.png");
+
+        final TextView applicationName = (TextView) view.findViewById(R.id.applicationName);
+        applicationName.setText(ApplicationHelper.name());
+
+        final TextView authorName = (TextView) view.findViewById(R.id.authorName);
+        authorName.setText("ShkSchneider");
 
         return view;
     }
