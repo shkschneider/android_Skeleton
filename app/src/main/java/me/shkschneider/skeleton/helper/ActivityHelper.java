304b4c1eb6890ef5a0eaae330917144771424939
shkschneider <shk.schneider@gmail.com>
2014-11-04 18:21:36 +0100

    Gradle: got rid off AppMsg as Crouton

--- app/src/main/java/me/shkschneider/skeleton/helper/ActivityHelper.java
+++ app/src/main/java/me/shkschneider/skeleton/helper/ActivityHelper.java
@@ -2,11 +2,8 @@ package me.shkschneider.skeleton.helper;
 
 import android.app.Activity;
 import android.content.res.Configuration;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
-
-import com.devspark.appmsg.AppMsg;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -22,23 +19,23 @@ public class ActivityHelper {
         Toast.makeText(MainApplication.CONTEXT, msg, Toast.LENGTH_SHORT).show();
     }
 
-    private static void crouton(@NotNull final Activity activity, @NotNull final String msg, final AppMsg.Style style) {
-        final AppMsg appMsg = AppMsg.makeText(activity, msg, style);
-        appMsg.setLayoutGravity(Gravity.BOTTOM);
-        appMsg.show();
-    }
-
-    public static void croutonGreen(@NotNull final Activity activity, @NotNull final String msg) {
-        crouton(activity, msg, AppMsg.STYLE_INFO);
-    }
-
-    public static void croutonOrange(@NotNull final Activity activity, @NotNull final String msg) {
-        crouton(activity, msg, AppMsg.STYLE_CONFIRM);
-    }
-
-    public static void croutonRed(@NotNull final Activity activity, @NotNull final String msg) {
-        crouton(activity, msg, AppMsg.STYLE_ALERT);
-    }
+//    private static void crouton(@NotNull final Activity activity, @NotNull final String msg, final AppMsg.Style style) {
+//        final AppMsg appMsg = AppMsg.makeText(activity, msg, style);
+//        appMsg.setLayoutGravity(Gravity.BOTTOM);
+//        appMsg.show();
+//    }
+//
+//    public static void croutonGreen(@NotNull final Activity activity, @NotNull final String msg) {
+//        crouton(activity, msg, AppMsg.STYLE_INFO);
+//    }
+//
+//    public static void croutonOrange(@NotNull final Activity activity, @NotNull final String msg) {
+//        crouton(activity, msg, AppMsg.STYLE_CONFIRM);
+//    }
+//
+//    public static void croutonRed(@NotNull final Activity activity, @NotNull final String msg) {
+//        crouton(activity, msg, AppMsg.STYLE_ALERT);
+//    }
 
     public static boolean portrait() {
         return (MainApplication.CONTEXT.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT);
