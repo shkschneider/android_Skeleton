aceaca8040915f9da3bb93d1d0b25d5a0068656e
shkschneider <shk.schneider@gmail.com>
2015-01-14 15:25:30 +0100

    MySwipeRefreshlayout: refreshable state in SkeletonActivity and SkeletonFragmentActivity

--- skeleton/src/main/java/me/shkschneider/skeleton/SkeletonFragmentActivity.java
+++ skeleton/src/main/java/me/shkschneider/skeleton/SkeletonFragmentActivity.java
@@ -18,4 +18,9 @@ public class SkeletonFragmentActivity extends SkeletonActivity {
                 .commit();
     }
 
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+    }
+
 }
