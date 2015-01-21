aac9f7e4a5bca81a58fb69a38a182479fac3e771
shkschneider <shk.schneider@gmail.com>
2015-01-21 10:33:22 +0100

    ViewPagerCircleIndicator: correct constructor and initializer

--- skeleton/src/main/java/me/shkschneider/skeleton/ui/viewpager/ViewPagerCircleIndicator.java
+++ skeleton/src/main/java/me/shkschneider/skeleton/ui/viewpager/ViewPagerCircleIndicator.java
@@ -2,6 +2,8 @@ package me.shkschneider.skeleton.ui.viewpager;
 
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
@@ -12,6 +14,7 @@ import android.view.animation.LinearInterpolator;
 import android.widget.LinearLayout;
 
 import me.shkschneider.skeleton.R;
+import me.shkschneider.skeleton.helper.AndroidHelper;
 import me.shkschneider.skeleton.helper.LogHelper;
 import me.shkschneider.skeleton.helper.ScreenHelper;
 
@@ -22,11 +25,11 @@ public class ViewPagerCircleIndicator extends LinearLayout implements ViewPager.
 
     private ViewPager mViewpager;
     private ViewPager.OnPageChangeListener mViewPagerOnPageChangeListener;
-    private int mIndicatorMargin = ScreenHelper.pixelsFromDp(DEFAULT_INDICATOR_WIDTH);
-    private int mIndicatorWidth = ScreenHelper.pixelsFromDp(DEFAULT_INDICATOR_WIDTH);
-    private int mIndicatorHeight = ScreenHelper.pixelsFromDp(DEFAULT_INDICATOR_WIDTH);
-    private int mAnimatorResId = R.animator.viewpager_circleindicator_scalealpha;
-    private int mIndicatorBackground = R.drawable.viewpager_circleindicator;
+    private int mIndicatorMargin;
+    private int mIndicatorWidth;
+    private int mIndicatorHeight;
+    private int mAnimatorResId;
+    private int mIndicatorBackground;
     private int mCurrentPosition = 0;
     private AnimatorSet mAnimationOut;
     private AnimatorSet mAnimationIn;
@@ -36,13 +39,31 @@ public class ViewPagerCircleIndicator extends LinearLayout implements ViewPager.
     }
 
     public ViewPagerCircleIndicator(final Context context, final AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
+    }
+
+    public ViewPagerCircleIndicator(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    @SuppressWarnings("NewApi")
+    @SuppressLint("NewApi")
+    @TargetApi(AndroidHelper.API_21)
+    public ViewPagerCircleIndicator(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
         init(context);
     }
 
     private void init(final Context context) {
         setOrientation(LinearLayout.HORIZONTAL);
         setGravity(Gravity.CENTER);
+        mIndicatorMargin = ScreenHelper.pixelsFromDp(DEFAULT_INDICATOR_WIDTH);
+        mIndicatorWidth = ScreenHelper.pixelsFromDp(DEFAULT_INDICATOR_WIDTH);
+        mIndicatorHeight = ScreenHelper.pixelsFromDp(DEFAULT_INDICATOR_WIDTH);
+        mAnimatorResId = R.animator.viewpager_circleindicator_scalealpha;
+        mIndicatorBackground = R.drawable.viewpager_circleindicator;
+        mCurrentPosition = 0;
         mAnimationOut = (AnimatorSet) AnimatorInflater.loadAnimator(context, mAnimatorResId);
         mAnimationOut.setInterpolator(new LinearInterpolator());
         mAnimationIn = (AnimatorSet) AnimatorInflater.loadAnimator(context, mAnimatorResId);
