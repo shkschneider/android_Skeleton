b989fc5218c8bc2080fa6c7b91a6bf08bf16adb8
shkschneider <shk.schneider@gmail.com>
2015-05-27 16:50:27 +0200

    AutoImageView

---
+++ library/src/main/java/me/shkschneider/skeleton/ui/AutoImageView.java
@@ -0,0 +1,35 @@
+package me.shkschneider.skeleton.ui;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+// http://stackoverflow.com/a/12283909
+public class AutoImageView extends ImageView {
+
+    public AutoImageView(final Context context) {
+        super(context);
+    }
+
+    public AutoImageView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoImageView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        final Drawable drawable = getDrawable();
+        if (drawable == null) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            return ;
+        }
+        final int width = MeasureSpec.getSize(widthMeasureSpec);
+        final int height = (int) Math.ceil((float) width * (float) drawable.getIntrinsicHeight() / (float) drawable.getIntrinsicWidth());
+        setMeasuredDimension(width, height);
+    }
+
+}
