15451628c7801e1048c02f266482c1d7c37ef85c
shkschneider <shk.schneider@gmail.com>
2015-06-18 15:56:22 +0200

    PNG: optimizes with pngquant (weight / 2, losseless)

---
+++ png.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+[ -x pngquant ] && echo "E: pngquant not found" >&2 && exit 1
+
+start=$(date +%s)
+
+sizeIn=0
+sizeOut=0
+while read dir ; do
+    sizeIn=$(($sizeIn + $(du -s "$dir" | awk '{print $1}')))
+    while read file ; do
+        echo "$file"
+        cp "$file" "/tmp/png"
+        pngquant -f "/tmp/png" ; [ $? -ne 0 ] && echo "ERROR" >&2 && exit 1 ; mv "/tmp/png-fs8.png" "$file"
+    done < <(find "$dir" -type f -name "*.png" -not -name "*.9.png" | grep -v "build")
+    sizeOut=$(($sizeOut + $(du -s "$dir" | awk '{print $1}')))
+done < <(find "." -type d -name "res" | grep -v "build")
+
+ratio=$(echo "100 - (($sizeOut * 100) / $sizeIn)" | bc -q)
+end=$(date +%s)
+time=$(echo "$end - $start" | bc -q)
+echo "+--------+"
+echo "| INPUT  | ${sizeIn}B"
+echo "| OUTPUT | ${sizeOut}B (-$ratio%)"
+echo "| TIME   | ${time}s"
+echo "+--------+"
+
+# EOF
