4d22eef8bb3881fd7d97976ab0c063f45b32117a
shkschneider <shk.schneider@gmail.com>
2016-01-12 16:29:17 +0100

    WebService: default timeouts, default cache, POST body as JSON

--- library/src/main/java/me/shkschneider/skeleton/network/WebService.java
+++ library/src/main/java/me/shkschneider/skeleton/network/WebService.java
@@ -7,28 +7,23 @@ import android.support.annotation.Size;
 
 import com.google.gson.JsonObject;
 
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
-import java.io.DataOutputStream;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Locale;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
-import me.shkschneider.skeleton.data.CharsetHelper;
 import me.shkschneider.skeleton.data.FileHelper;
 import me.shkschneider.skeleton.data.GsonParser;
+import me.shkschneider.skeleton.helper.LocaleHelper;
 import me.shkschneider.skeleton.helper.LogHelper;
 import me.shkschneider.skeleton.java.ClassHelper;
-import me.shkschneider.skeleton.java.StringHelper;
 
 // <http://developer.android.com/reference/java/net/HttpURLConnection.html>
 public class WebService extends AsyncTask<Void, Void, Object> {
-
-    private static final int TIMEOUT_CONNECT = (int) TimeUnit.SECONDS.toMillis(3);
-    private static final int TIMEOUT_READ = (int) TimeUnit.SECONDS.toMillis(5);
-    private static final int CACHE = (int) TimeUnit.HOURS.toMillis(24);
 
     private Method mMethod;
     private String mUrl;
@@ -77,9 +72,8 @@ public class WebService extends AsyncTask<Void, Void, Object> {
             final URL url = new URL(mUrl);
             // Connection
             httpURLConnection = (HttpURLConnection) url.openConnection();
-            // Timeouts
-            httpURLConnection.setConnectTimeout(TIMEOUT_CONNECT);
-            httpURLConnection.setReadTimeout(TIMEOUT_READ);
+            // httpURLConnection.setConnectTimeout(TIMEOUT_CONNECT);
+            // httpURLConnection.setReadTimeout(TIMEOUT_READ);
             // Method
             final String method = mMethod.toString();
             if (method == null) {
@@ -88,23 +82,27 @@ public class WebService extends AsyncTask<Void, Void, Object> {
             httpURLConnection.setRequestMethod(method);
             httpURLConnection.setDoOutput(httpURLConnection.getRequestMethod().equals("POST"));
             // Cache
-            httpURLConnection.addRequestProperty("Cache-Control", (CACHE < 0 ? "no-cache" : "max-stale=" + CACHE));
+            // httpURLConnection.addRequestProperty("Cache-Control", (CACHE < 0 ? "no-cache" : "max-stale=" + CACHE));
             // Parameters
             if (mBody != null) {
-                final DataOutputStream dataOutputStream = new DataOutputStream(httpURLConnection.getOutputStream());
-                String params = "";
-                for (final String key : mBody.keySet()) {
-                    if (! StringHelper.nullOrEmpty(params)) {
-                        params += "&";
-                    }
-                    params += key + "=" + UrlHelper.encode(mBody.get(key));
-                }
-                dataOutputStream.write(params.getBytes(CharsetHelper.UTF8));
+//                final DataOutputStream dataOutputStream = new DataOutputStream(httpURLConnection.getOutputStream());
+//                String params = "";
+//                for (final String key : mBody.keySet()) {
+//                    if (! StringHelper.nullOrEmpty(params)) {
+//                        params += "&";
+//                    }
+//                    params += key + "=" + UrlHelper.encode(mBody.get(key));
+//                }
+//                dataOutputStream.write(params.getBytes(CharsetHelper.UTF8));
+                final OutputStreamWriter outputStreamWriter = new OutputStreamWriter(httpURLConnection.getOutputStream());
+                final JSONObject body = new JSONObject(mBody);
+                outputStreamWriter.write(body.toString());
+                outputStreamWriter.close();
             }
             // Response
             final int responseCode = httpURLConnection.getResponseCode();
             final String responseMessage = httpURLConnection.getResponseMessage();
-            LogHelper.debug(String.format(Locale.US, "%d: %s", responseCode, responseMessage));
+            LogHelper.debug(String.format(LocaleHelper.locale(), "%d: %s", responseCode, responseMessage));
             InputStream inputStream;
             switch (responseCode) {
                 case HttpURLConnection.HTTP_OK:
