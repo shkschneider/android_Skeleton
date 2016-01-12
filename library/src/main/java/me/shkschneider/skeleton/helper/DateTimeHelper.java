fe7e990ac3902543e098aea19ded67374a74c3c9
shkschneider <shk.schneider@gmail.com>
2016-01-12 16:26:16 +0100

    DateTimeHelper: reworked with Calendar

--- library/src/main/java/me/shkschneider/skeleton/helper/DateTimeHelper.java
+++ library/src/main/java/me/shkschneider/skeleton/helper/DateTimeHelper.java
@@ -1,13 +1,17 @@
 package me.shkschneider.skeleton.helper;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.format.DateFormat;
 import android.text.format.DateUtils;
+import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Locale;
 import java.util.TimeZone;
 
 public class DateTimeHelper {
@@ -19,28 +23,35 @@ public class DateTimeHelper {
         // Empty
     }
 
+    // <http://howtodoinjava.com/2012/12/16/always-use-setlenient-false-when-using-simpledateformat-for-date-validation-in-java/>
+    public static Calendar calendar() {
+        final Calendar calendar = Calendar.getInstance(LocaleHelper.locale());
+        calendar.setLenient(false);
+        return calendar;
+    }
+
     public static int day() {
-        return Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        return calendar().get(Calendar.DAY_OF_MONTH);
     }
 
     public static int month() {
-        return Calendar.getInstance().get(Calendar.MONTH) + 1;
+        return calendar().get(Calendar.MONTH) + 1;
     }
 
     public static int year() {
-        return Calendar.getInstance().get(Calendar.YEAR);
+        return calendar().get(Calendar.YEAR);
     }
 
     public static String format(@NonNull final Calendar calendar, @NonNull final String format) {
-        return new SimpleDateFormat(format, Locale.US).format(calendar.getTime());
+        return new SimpleDateFormat(format, LocaleHelper.locale()).format(calendar.getTime());
     }
 
     public static long timestamp() {
-        return Calendar.getInstance().getTimeInMillis() / 1000;
+        return calendar().getTimeInMillis() / 1000;
     }
 
     public static long milliTimestamp() {
-        return Calendar.getInstance().getTimeInMillis();
+        return calendar().getTimeInMillis();
     }
 
     public static int gmtOffset() {
@@ -54,6 +65,17 @@ public class DateTimeHelper {
     @Nullable
     public static String relative(@IntRange(from=0) final long from, @IntRange(from=0) final long to) {
         return DateUtils.getRelativeTimeSpanString(from, to, DateUtils.SECOND_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE).toString();
+    }
+
+    public static void pickTime(@NonNull final Activity activity, final Calendar date, @NonNull final TimePickerDialog.OnTimeSetListener onTimeSetListener) {
+        final Calendar calendar = (date != null ? date : calendar());
+        final boolean is24HourFormat = DateFormat.is24HourFormat(ApplicationHelper.context());
+        new TimePickerDialog(activity, onTimeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), is24HourFormat).show();
+    }
+
+    public static void pickDate(@NonNull final Activity activity, final Calendar date, @NonNull final DatePickerDialog.OnDateSetListener onDateSetListener) {
+        final Calendar calendar = (date != null ? date : calendar());
+        new DatePickerDialog(activity, onDateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 
 }
