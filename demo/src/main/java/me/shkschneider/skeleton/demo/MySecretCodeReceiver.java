55d8d9c5b13701654347a2960cf4799308d4b5ab
shkschneider <shk.schneider@gmail.com>
2016-01-12 16:23:24 +0100

    MySecretCodeReceiver POC

---
+++ demo/src/main/java/me/shkschneider/skeleton/demo/MySecretCodeReceiver.java
@@ -0,0 +1,21 @@
+package me.shkschneider.skeleton.demo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import me.shkschneider.skeleton.demo.activity.MainActivity;
+
+// <https://github.com/SimonMarquis/Android-SecretCodes>
+public class MySecretCodeReceiver extends BroadcastReceiver {
+
+    private static final String ACTION = "android.provider.Telephony.SECRET_CODE";
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        if (intent.getAction().equals(ACTION)) {
+            context.startActivity(new Intent(context, MainActivity.class));
+        }
+    }
+
+}
