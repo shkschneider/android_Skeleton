1eb5f65881d8ec234a18634231cea5313c2fe8cd
shkschneider <shk.schneider@gmail.com>
2016-01-12 16:26:52 +0100

    AutoGridViewHeight+AutoListViewHeight

---
+++ library/src/main/java/me/shkschneider/skeleton/ui/AutoGridViewHeight.java
@@ -0,0 +1,48 @@
+package me.shkschneider.skeleton.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+// <http://stackoverflow.com/a/8483078>
+public class AutoGridViewHeight extends GridView {
+
+    private boolean mExpanded = false;
+
+    public AutoGridViewHeight(final Context context) {
+        super(context);
+    }
+
+    public AutoGridViewHeight(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoGridViewHeight(final Context context, final AttributeSet attrs, final int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public boolean isExpanded() {
+        return mExpanded;
+    }
+
+    @Override
+    public void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        if (isExpanded()) {
+            // Calculate entire height by providing a very large height hint.
+            // View.MEASURED_SIZE_MASK represents the largest height possible.
+            final int expandSpec = MeasureSpec.makeMeasureSpec(MEASURED_SIZE_MASK, MeasureSpec.AT_MOST);
+            super.onMeasure(widthMeasureSpec, expandSpec);
+            final ViewGroup.LayoutParams params = getLayoutParams();
+            params.height = getMeasuredHeight();
+        }
+        else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    public void setExpanded(final boolean expanded) {
+        mExpanded = expanded;
+    }
+
+}
