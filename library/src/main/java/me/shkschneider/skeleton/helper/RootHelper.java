2dca9998c0f90e83b9638a61c1c436c7db9417e2
shkschneider <shk.schneider@gmail.com>
2016-01-22 17:22:41 +0100

    RootHelper.rooted()

--- library/src/main/java/me/shkschneider/skeleton/helper/RootHelper.java
+++ library/src/main/java/me/shkschneider/skeleton/helper/RootHelper.java
@@ -7,29 +7,36 @@ import java.util.List;
 // <https://github.com/orhanobut/rootchecker/blob/master/rootchecker/src/main/java/com/orhanobut/rootchecker/RootChecker.java>
 public class RootHelper {
 
-    @Deprecated
-    public static boolean rooted() {
+    private static String binary() {
         final List<String> paths = new ArrayList<String>() {
             {
-                add("/data/local/bin/su");
-                add("/data/local/su");
-                add("/data/local/xbin/su");
-                add("/sbin/su");
-                add("/system/bin/failsafe/su");
-                add("/system/bin/su");
-                add("/system/sd/xbin/su");
-                add("/system/xbin/su");
+                add("/sbin");
+                add("/system/bin");
+                add("/system/xbin");
+                add("/data/local/xbin");
+                add("/data/local/bin");
+                add("/system/sd/xbin");
+                add("/system/bin/failsafe");
+                add("/data/local");
             }
         };
         for (final String path : paths) {
-            final File file = new File(path);
+            final File file = new File(path + SystemProperties.property(SystemProperties.SYSTEM_PROPERTY_FILE_SEPARATOR) + "su");
             if (file.exists() && file.isFile() && file.canExecute()) {
-                return true;
+                return file.getAbsolutePath();
             }
         }
-        return false;
+        return null;
     }
 
-    // TODO run()
+    public static boolean rooted() {
+        return (binary() != null);
+    }
+
+    // Careful
+    @Deprecated
+    public static void run(final String commandLine) {
+        // TODO
+    }
 
 }
