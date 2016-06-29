e28b7cabcf460580242659a285f51e882bd03b54
shkschneider <shk.schneider@gmail.com>
2016-06-29 15:23:57 +0200

    DeviceHelper: cleaner emulator detection, better attempt to detect tablet by trying to evaluate screenSize() in inches ; removed id()

--- library/src/main/java/me/shkschneider/skeleton/helper/DeviceHelper.java
+++ library/src/main/java/me/shkschneider/skeleton/helper/DeviceHelper.java
@@ -1,7 +1,8 @@
 package me.shkschneider.skeleton.helper;
 
-import android.content.res.Configuration;
 import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
 // <http://developer.android.com/reference/android/os/Build.html>
 public class DeviceHelper {
@@ -10,10 +11,24 @@ public class DeviceHelper {
         // Empty
     }
 
+    // <http://stackoverflow.com/a/15133818>
+    @Deprecated // Avoid
+    public static double screenSize() {
+        final WindowManager windowManager = SystemServices.windowManager();
+        final DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        final int h = displayMetrics.heightPixels;
+        final int w = displayMetrics.widthPixels;
+        final double diagonalPixels = Math.sqrt(Math.pow(h, 2) + Math.pow(w, 2));
+        return diagonalPixels / displayMetrics.densityDpi;
+    }
+
+    @SuppressWarnings("deprecation")
     @Deprecated // Avoid
     public static boolean tablet() {
-        final int screenLayout = ApplicationHelper.resources().getConfiguration().screenLayout;
-        return ((screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE);
+        final double screenSize = screenSize();
+        LogHelper.verbose("screenSize: " + screenSize + "\"");
+        return (screenSize >= 7.0);
     }
 
     // <https://github.com/framgia/android-emulator-detector>
@@ -40,20 +55,22 @@ public class DeviceHelper {
         emulator = Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic");
         if (emulator) return true;
         emulator = Build.PRODUCT.equals("google_sdk");
+//        final TelephonyManager telephonyManager = SystemServices.telephonyManager();
+//        if (telephonyManager != null) {
+//            if (new ArrayList<String>() {
+//                {
+//                    add("000000000000000");
+//                    add("e21833235b6eef10");
+//                    add("012345678912345");
+//                }
+//            }.contains(telephonyManager.getDeviceId())) {
+//                return true;
+//            }
+//        }
         return emulator;
 
     }
 
-    public static String architecture() {
-        return SystemProperties.property(SystemProperties.SYSTEM_PROPERTY_OS_ARCH);
-    }
-
-    // End-user-friendly
-    public static String model() {
-        return Build.MODEL;
-    }
-
-    // End-user-friendly
     public static String brand() {
         return Build.BRAND;
     }
@@ -62,12 +79,16 @@ public class DeviceHelper {
         return Build.MANUFACTURER;
     }
 
+    public static String model() {
+        return Build.MODEL;
+    }
+
     public static String codename() {
         return Build.DEVICE;
     }
 
-    public static String id() {
-        return Build.ID;
+    public static String architecture() {
+        return SystemProperties.property(SystemProperties.SYSTEM_PROPERTY_OS_ARCH);
     }
 
     @Deprecated // Avoid
