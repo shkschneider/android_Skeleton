1fc1bc7ced4f124cacf76f0d0de6b6a9597b11be
shkschneider <shk.schneider@gmail.com>
2016-09-09 12:14:11 +0200

    ThreadHelper

---
+++ library/src/main/java/me/shkschneider/skeleton/helper/ThreadHelper.java
@@ -0,0 +1,39 @@
+package me.shkschneider.skeleton.helper;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.FutureTask;
+
+public class ThreadHelper {
+
+    protected ThreadHelper() {
+        // Empty
+    }
+
+    public static boolean uiThread() {
+        return  (Looper.getMainLooper() == Looper.myLooper());
+    }
+
+    // android.support.annotation.UiThread
+    public static void foregroundThread(@NonNull final Runnable runnable) {
+        if (uiThread()) {
+            runnable.run();
+            return;
+        }
+        new Handler(Looper.getMainLooper()).post(runnable);
+    }
+
+    public static FutureTask backgroundThread(@NonNull final Runnable runnable) {
+        final FutureTask<Void> futureTask = new FutureTask<>(new Runnable() {
+            @Override
+            public void run() {
+                new Thread(runnable).start();
+            }
+        }, null);
+        futureTask.run();
+        return futureTask;
+    }
+
+}
