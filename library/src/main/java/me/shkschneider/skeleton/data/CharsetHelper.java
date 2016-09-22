94a6d1f5795e4014dd739cd4f841ca9ad76ac0e7
shkschneider <shk.schneider@gmail.com>
2016-09-22 18:06:19 +0200

    Charset documentation

--- library/src/main/java/me/shkschneider/skeleton/data/CharsetHelper.java
+++ library/src/main/java/me/shkschneider/skeleton/data/CharsetHelper.java
@@ -6,10 +6,10 @@ public class CharsetHelper {
         // Empty
     }
 
-    // <https://hc.apache.org/httpcomponents-core-dev/httpcore/apidocs/constant-values.html#org.apache.http.protocol.HTTP.US_ASCII>
-    public static final String ASCII = "US-ASCII"; // StandardCharsets.US_ASCII.name();
-    public static final String UTF8 = "UTF-8"; // StandardCharsets.UTF_8.name();
-    public static final String UTF16 = "UTF-16"; // StandardCharsets.UTF_16.name();
-    public static final String ISO_8859_1 = "ISO-8859-1"; // StandardCharsets.ISO_8859_1.name();
+    // <https://docs.oracle.com/javase/7/docs/api/java/nio/charset/StandardCharsets.html>
+    public static final String ASCII = "US-ASCII";
+    public static final String UTF8 = "UTF-8";
+    public static final String UTF16 = "UTF-16";
+    public static final String ISO_8859_1 = "ISO-8859-1";
 
 }
