8a58ae6a3cad5f5cc3a6e713cc33c2bd7b67a9a3
shkschneider <shk.schneider@gmail.com>
2017-07-26 17:55:02 +0200

    StrictModeHelper: ThreadPolicy and VmPolicy

---
+++ library/src/main/java/me/shkschneider/skeleton/java/StrictModeHelper.java
@@ -0,0 +1,45 @@
+package me.shkschneider.skeleton.java;
+
+import android.os.StrictMode;
+
+import me.shkschneider.skeleton.helper.ApplicationHelper;
+
+public class StrictModeHelper {
+
+    public static void on() {
+        on(false);
+    }
+
+    public static void on(final boolean death) {
+        if (! ApplicationHelper.debug()) {
+            return;
+        }
+
+        final StrictMode.ThreadPolicy.Builder threadPolicyBuilder = new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog();
+        if (death) {
+            // threadPolicyBuilder.penaltyDialog();
+            threadPolicyBuilder.penaltyDeath();
+        }
+        StrictMode.setThreadPolicy(threadPolicyBuilder.build());
+
+        final StrictMode.VmPolicy.Builder vmPolicyBuilder = new StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog();
+        if (death) {
+            vmPolicyBuilder.penaltyDeath();
+        }
+        StrictMode.setVmPolicy(vmPolicyBuilder.build());
+    }
+
+    public static void off() {
+        if (! ApplicationHelper.debug()) {
+            return;
+        }
+
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.LAX);
+        StrictMode.setVmPolicy(StrictMode.VmPolicy.LAX);
+    }
+
+}
