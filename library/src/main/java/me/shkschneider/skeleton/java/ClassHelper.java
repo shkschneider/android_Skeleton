9907341a707e7b503eafd12a24b7577d25cfd34e
shkschneider <shk.schneider@gmail.com>
2017-08-18 16:05:30 +0200

    ClassHelper: generic class specified

--- library/src/main/java/me/shkschneider/skeleton/java/ClassHelper.java
+++ library/src/main/java/me/shkschneider/skeleton/java/ClassHelper.java
@@ -22,15 +22,15 @@ public class ClassHelper {
         }
     }
 
-    public static String canonicalName(@NonNull final Class cls) {
+    public static String canonicalName(@NonNull final Class<?> cls) {
         return cls.getCanonicalName();
     }
 
-    public static String packageName(@NonNull final Class cls) {
+    public static String packageName(@NonNull final Class<?> cls) {
         return canonicalName(cls).replaceFirst("\\.[^\\.]+$", "");
     }
 
-    public static String simpleName(@NonNull final Class cls) {
+    public static String simpleName(@NonNull final Class<?> cls) {
         return cls.getSimpleName();
     }
 
