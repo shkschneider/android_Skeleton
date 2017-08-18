cafff97c39453a05f85138c7a121304146d38eb3
shkschneider <shk.schneider@gmail.com>
2017-08-18 11:43:48 +0200

    ExceptionHelper: Thread.setDefaultUncaughtExceptionHandler()

--- library/src/main/java/me/shkschneider/skeleton/SkeletonApplication.java
+++ library/src/main/java/me/shkschneider/skeleton/SkeletonApplication.java
@@ -4,9 +4,11 @@ import android.app.Application;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 
+import me.shkschneider.skeleton.helper.ApplicationHelper;
 import me.shkschneider.skeleton.helper.ContextHelper;
 import me.shkschneider.skeleton.helper.DeviceHelper;
 import me.shkschneider.skeleton.helper.LogHelper;
+import me.shkschneider.skeleton.java.ExceptionHelper;
 
 /**
  * https://developer.android.com/reference/android/app/Application.html
@@ -29,6 +31,15 @@ public abstract class SkeletonApplication extends Application {
         final ApplicationInfo applicationInfo = getApplicationInfo();
         DEBUG = ((applicationInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0);
         TAG = applicationInfo.packageName;
+
+        if (ApplicationHelper.debug()) {
+            ExceptionHelper.uncaughtException(new ExceptionHelper.Callback() {
+                @Override
+                public void uncaughtException(final Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            });
+        }
 
         LogHelper.verbose("Hello, " + DeviceHelper.codename() + "!");
     }
