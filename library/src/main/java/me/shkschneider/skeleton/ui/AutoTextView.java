3b824afe8bc21c74564b283fa7a4e3294997962b
shkschneider <shk.schneider@gmail.com>
2017-08-19 15:45:27 +0200

    TextViewHelper.sizeToFit()

--- library/src/main/java/me/shkschneider/skeleton/ui/AutoTextView.java
+++ library/src/main/java/me/shkschneider/skeleton/ui/AutoTextView.java
@@ -1,13 +1,9 @@
 package me.shkschneider.skeleton.ui;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Rect;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 
-// <http://jeffreysambells.com/2010/04/04/android-textview-with-auto-sized-content>
 public class AutoTextView extends AppCompatTextView {
 
     public AutoTextView(final Context context) {
@@ -20,25 +16,23 @@ public class AutoTextView extends AppCompatTextView {
 
     public AutoTextView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TextViewHelper.sizeToFit(this, true);
     }
 
-    @Override
-    protected void onDraw(@NonNull final Canvas canvas) {
-        super.onDraw(canvas);
-        sizeToFit();
-    }
-
-    public void sizeToFit() {
-        final int height = getHeight();
-        final int lines = getLineCount();
-        final Rect rect = new Rect();
-        // final int y1 = getLineBounds(0, rect);
-        final int y2 = getLineBounds(lines - 1, rect);
-        final float size = getTextSize();
-        if (y2 > height && size >= 8.0F) {
-            setTextSize(size - 2.0F);
-            sizeToFit();
-        }
-    }
+    // <http://jeffreysambells.com/2010/04/04/android-textview-with-auto-sized-content>
+//    @Override
+//    protected void onDraw(@NonNull final Canvas canvas) {
+//        super.onDraw(canvas);
+//        final int height = getHeight();
+//        final int lines = getLineCount();
+//        final Rect rect = new Rect();
+//        // final int y1 = getLineBounds(0, rect);
+//        final int y2 = getLineBounds(lines - 1, rect);
+//        final float size = getTextSize();
+//        if (y2 > height && size >= 8.0F) {
+//            setTextSize(size - 2.0F);
+//            sizeToFit();
+//        }
+//    }
 
 }
--- library/src/main/java/me/shkschneider/skeleton/ui/TextViewHelper.java
+++ library/src/main/java/me/shkschneider/skeleton/ui/TextViewHelper.java
@@ -1,6 +1,7 @@
 package me.shkschneider.skeleton.ui;
 
 import android.support.annotation.NonNull;
+import android.support.v4.widget.TextViewCompat;
 import android.text.Html;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -22,7 +23,7 @@ public class TextViewHelper {
         return Html.fromHtml(html, new MyImageGetter(textView), null);
     }
 
-    public static void linkify(final TextView textView, final boolean urls, final boolean emails, final boolean phones, final boolean addresses) {
+    public static void linkify(@NonNull final TextView textView, final boolean urls, final boolean emails, final boolean phones, final boolean addresses) {
         int links = 0;
         if (urls) links |= Linkify.WEB_URLS;
         if (emails) links |= Linkify.EMAIL_ADDRESSES;
@@ -34,11 +35,16 @@ public class TextViewHelper {
         textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
-    public static void linkify(final TextView textView) {
+    public static void linkify(@NonNull final TextView textView) {
         final SpannableString spannableString = new SpannableString(textView.getText());
         Linkify.addLinks(spannableString, Linkify.ALL);
         textView.setText(spannableString);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    public static void sizeToFit(@NonNull final TextView textView, final boolean autoSize) {
+        final int autoSizeTextType = (autoSize ? TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM : TextViewCompat.AUTO_SIZE_TEXT_TYPE_NONE);
+        TextViewCompat.setAutoSizeTextTypeWithDefaults(textView, autoSizeTextType);
     }
 
 }
