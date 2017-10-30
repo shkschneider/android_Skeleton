84086400ecf7264dc6336d5cfb32ac1c629ce4cb
shkschneider <shk.schneider@gmail.com>
2017-10-30 10:02:05 +0100

    ActivityHelper: pendingTransitions

--- library/src/main/java/me/shkschneider/skeleton/helper/ActivityHelper.java
+++ library/src/main/java/me/shkschneider/skeleton/helper/ActivityHelper.java
@@ -1,5 +1,6 @@
 package me.shkschneider.skeleton.helper;
 
+import android.app.ActivityOptions;
 import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.view.View;
@@ -34,6 +35,16 @@ public class ActivityHelper {
     @Deprecated
     public static void snackBar(@NonNull final View view, @NonNull final String msg, @NonNull final String action, @NonNull final View.OnClickListener onClickListener) {
         Snack.bar(view, msg, action, onClickListener);
+    }
+
+    // <https://developer.android.com/reference/android/app/ActivityOptions.html>
+
+    public static ActivityOptions pendingTransition(int in, int out) {
+        return ActivityOptions.makeCustomAnimation(ContextHelper.applicationContext(), in, out);
+    }
+
+    public static ActivityOptions pendingTransition() {
+        return pendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
 }
