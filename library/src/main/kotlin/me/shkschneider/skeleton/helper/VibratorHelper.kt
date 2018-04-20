c665dbc704933c28756bdab28ec1638b75cd51dd
shkschneider <shk.schneider@gmail.com>
2018-04-20 11:38:00 +0200

    VibratorHelper: kotlin way

--- library/src/main/kotlin/me/shkschneider/skeleton/helper/VibratorHelper.kt
+++ library/src/main/kotlin/me/shkschneider/skeleton/helper/VibratorHelper.kt
@@ -1,7 +1,6 @@
 package me.shkschneider.skeleton.helper
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.os.Build
 import android.os.VibrationEffect
 import android.support.annotation.RequiresPermission
@@ -13,22 +12,17 @@ object VibratorHelper {
     private val DO_NOT_REPEAT = -1
 
     fun has(): Boolean {
-        val vibrator = SystemServices.vibrator()
-        return vibrator != null && vibrator.hasVibrator()
+        return SystemServices.vibrator()?.let { return it.hasVibrator() } ?: false
     }
 
     @RequiresPermission(Manifest.permission.VIBRATE)
-    fun vibrate(durations: LongArray, repeat: Boolean = false) {
+    fun vibrate(durations: LongArray = DEFAULT_DURATION, repeat: Boolean = false) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             SystemServices.vibrator()?.vibrate(VibrationEffect.createWaveform(durations, if (repeat) DO_REPEAT else DO_NOT_REPEAT))
         } else {
+            @Suppress("DEPRECATION")
             SystemServices.vibrator()?.vibrate(durations, if (repeat) DO_REPEAT else DO_NOT_REPEAT)
         }
-    }
-
-    @RequiresPermission(Manifest.permission.VIBRATE)
-    fun vibrate() {
-        vibrate(DEFAULT_DURATION)
     }
 
 }
