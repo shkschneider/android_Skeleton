3922da324c7f5eefaa7ce723e1472fcd3a7e09a2
shkschneider <shk.schneider@gmail.com>
2018-04-26 16:18:01 +0200

    Kotlin extensions around KClasses

---
+++ library/src/main/kotlin/me/shkschneider/skeleton/extensions/_Android.kt
@@ -0,0 +1,26 @@
+package me.shkschneider.skeleton.extensions
+
+import android.content.Context
+import android.net.Uri
+import android.support.annotation.RequiresApi
+import me.shkschneider.skeleton.helper.AndroidHelper
+import kotlin.reflect.KClass
+
+fun <T:Any> Intent(context: Context, klass: KClass<T>):
+        android.content.Intent = android.content.Intent(context, klass.java)
+
+fun <T:Any> Intent(action: String, uri: Uri, context: Context, klass: KClass<T>):
+        android.content.Intent = android.content.Intent(action, uri, context, klass.java)
+
+fun <T:Any> ComponentName(pkg: Context, klass: KClass<T>):
+        android.content.ComponentName = android.content.ComponentName(pkg, klass.java)
+
+@RequiresApi(AndroidHelper.API_23)
+fun <T:Any> Context.getSystemService(klass: KClass<T>): T? {
+    return getSystemService(klass.java) ?: null
+}
+
+@RequiresApi(AndroidHelper.API_23)
+fun <T:Any> Context.getSystemServiceName(klass: KClass<T>): String? {
+    return getSystemServiceName(klass.java) ?: null
+}
--- library/src/main/kotlin/me/shkschneider/skeleton/extensions/_Any.kt
+++ library/src/main/kotlin/me/shkschneider/skeleton/extensions/_Any.kt
@@ -1,6 +1,27 @@
 package me.shkschneider.skeleton.extensions
 
+import kotlin.reflect.KClass
+
+// FIXME ClassHelper
+// FIXME ObjectHelper
+// FIXME ReflectionHelper
+
+// Uses no reflection
+fun <T:Any> KClass<T>.simpleName(): String {
+    return this.java.simpleName.orEmpty()
+}
+// Uses no reflection
+fun <T:Any> KClass<T>.qualifiedName(): String {
+    return this.java.name.orEmpty()
+}
+// Uses no reflection
+fun <T:Any> KClass<T>.packageName(): String {
+    return this.java.name?.substringBeforeLast(".").orEmpty()
+}
+
 // Avoids nulls to be translated to "null"
+// On String, use .orEmpty().toString()
+// This extension work on Any?
 fun Any?.toStringOrEmpty(): String {
     return this?.toString().orEmpty()
 }
