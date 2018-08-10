c05b65c1a2c445be75142ab84512c6652920b72d
shkschneider <shk.schneider@gmail.com>
2018-08-10 13:24:00 +0200

    LoadingProgressBar: color fixed depending on isIndeterminate

--- library/src/main/java/me/shkschneider/skeleton/ui/LoadingProgressBar.kt
+++ library/src/main/java/me/shkschneider/skeleton/ui/LoadingProgressBar.kt
@@ -1,6 +1,9 @@
 package me.shkschneider.skeleton.ui
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.support.annotation.ColorInt
+import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v4.widget.ContentLoadingProgressBar
 import android.util.AttributeSet
 
@@ -13,5 +16,13 @@ class LoadingProgressBar : ContentLoadingProgressBar {
     }
 
     constructor(context: Context, attrs: AttributeSet? = null) : super(context, attrs)
+
+    fun color(@ColorInt color: Int) {
+        if (isIndeterminate) {
+            DrawableCompat.setTintList(indeterminateDrawable, ColorStateList.valueOf(color))
+        } else {
+            DrawableCompat.setTintList(progressDrawable, ColorStateList.valueOf(color))
+        }
+    }
 
 }
