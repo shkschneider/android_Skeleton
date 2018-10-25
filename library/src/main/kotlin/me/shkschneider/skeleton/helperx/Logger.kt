cbdd79202451933d5db59bca88410f39c8650bdb
shkschneider <shk.schneider@gmail.com>
2018-10-25 10:49:46 +0200

    Logger: allows multiple lines and chaining

--- library/src/main/kotlin/me/shkschneider/skeleton/helperx/Logger.kt
+++ library/src/main/kotlin/me/shkschneider/skeleton/helperx/Logger.kt
@@ -27,41 +27,49 @@ object Logger {
                 }
             }
         }
-        when (level) {
-            VERBOSE -> Log.v(tag, prefix + msg, throwable)
-            DEBUG -> Log.d(tag, prefix + msg, throwable)
-            INFO -> Log.i(tag, prefix + msg, throwable)
-            WARN -> Log.w(tag, prefix + msg, throwable)
-            ERROR -> Log.e(tag, prefix + msg, throwable)
-            WTF -> Log.wtf(tag, prefix + msg, throwable)
+        msg?.split("\n")?.forEach { line ->
+            when (level) {
+                VERBOSE -> Log.v(tag, prefix + line, throwable)
+                DEBUG -> Log.d(tag, prefix + line, throwable)
+                INFO -> Log.i(tag, prefix + line, throwable)
+                WARN -> Log.w(tag, prefix + line, throwable)
+                ERROR -> Log.e(tag, prefix + line, throwable)
+                WTF -> Log.wtf(tag, prefix + line, throwable)
+            }
         }
     }
 
     // Debug logs are compiled in but stripped at runtime.
-    fun debug(msg: String, throwable: Throwable? = null) {
+    fun debug(msg: String, throwable: Throwable? = null): Logger {
         log(DEBUG, msg, throwable)
+        return this
     }
 
     // You should never compile verbose logs into your app, except during development.
-    fun verbose(msg: String, throwable: Throwable? = null) {
+    fun verbose(msg: String, throwable: Throwable? = null): Logger {
         log(VERBOSE, msg, throwable)
+        return this
     }
 
-    fun info(msg: String, throwable: Throwable? = null) {
+    fun info(msg: String, throwable: Throwable? = null): Logger {
         log(INFO, msg, throwable)
+        return this
     }
 
-    fun warning(msg: String, throwable: Throwable? = null) {
+    fun warning(msg: String, throwable: Throwable? = null): Logger {
         log(WARN, msg, throwable)
+        return this
     }
 
-    fun error(msg: String, throwable: Throwable? = null) {
+    fun error(msg: String, throwable: Throwable? = null): Logger {
         log(ERROR, msg, throwable)
+        return this
     }
 
     // Useful to log exceptions (avoids Exception.printStackTrace())
-    fun wtf(throwable: Throwable) { // "What a Terrible Failure"
+    fun wtf(throwable: Throwable): Logger { // "What a Terrible Failure"
         log(WTF, throwable.message, throwable)
+        return this
     }
 
 }
