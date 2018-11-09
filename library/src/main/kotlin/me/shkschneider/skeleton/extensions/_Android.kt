ea945271a60ae1329687e917b86c24433e365c1d
shkschneider <shk.schneider@gmail.com>
2018-11-09 10:28:22 +0100

    Extension: ViewGroup.addOrUpdateView()

--- library/src/main/kotlin/me/shkschneider/skeleton/extensions/_Android.kt
+++ library/src/main/kotlin/me/shkschneider/skeleton/extensions/_Android.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
 import android.net.Uri
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import me.shkschneider.skeleton.helper.AndroidHelper
 import kotlin.reflect.KClass
@@ -45,4 +47,14 @@ fun <T:Any> Context.getSystemService(klass: KClass<T>): T? {
 @RequiresApi(AndroidHelper.API_23)
 fun <T:Any> Context.getSystemServiceName(klass: KClass<T>): String? {
     return getSystemServiceName(klass.java) ?: null
+}
+
+// View
+
+fun ViewGroup.addOrUpdateView(view: View) {
+    // Avoids IllegalStateException for already added views
+    if (view.parent != null) {
+       this.removeView(view)
+    }
+    this.addView(view)
 }
