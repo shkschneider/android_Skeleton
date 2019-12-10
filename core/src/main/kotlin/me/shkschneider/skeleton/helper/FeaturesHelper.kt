1cca7b826e4ca643c0afd349e292ec4b239793b6
shkshkschneider <shk.schneider@gmail.com>
2019-12-10 17:50:34 +0100

    minSdkVersion 23

--- core/src/main/kotlin/me/shkschneider/skeleton/helper/FeaturesHelper.kt
+++ core/src/main/kotlin/me/shkschneider/skeleton/helper/FeaturesHelper.kt
@@ -9,47 +9,32 @@ object FeaturesHelper {
     // Useless: only used to keep track of new features
     @RequiresApi(AndroidHelper.API_26)
     const val FEATURE_ACTIVITIES_ON_SECONDARY_DISPLAYS = PackageManager.FEATURE_ACTIVITIES_ON_SECONDARY_DISPLAYS
-    @RequiresApi(AndroidHelper.API_18)
     const val FEATURE_APP_WIDGETS = PackageManager.FEATURE_APP_WIDGETS
     const val FEATURE_AUDIO_LOW_LATENCY = PackageManager.FEATURE_AUDIO_LOW_LATENCY
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_AUDIO_OUTPUT = PackageManager.FEATURE_AUDIO_OUTPUT
-    @RequiresApi(AndroidHelper.API_23)
     const val FEATURE_AUDIO_PRO = PackageManager.FEATURE_AUDIO_PRO
     @RequiresApi(AndroidHelper.API_26)
     const val FEATURE_AUTOFILL = PackageManager.FEATURE_AUTOFILL
-    @RequiresApi(AndroidHelper.API_23)
     const val FEATURE_AUTOMOTIVE = PackageManager.FEATURE_AUTOMOTIVE
-    @RequiresApi(AndroidHelper.API_20)
     const val FEATURE_BACKUP = PackageManager.FEATURE_BACKUP
     const val FEATURE_BLUETOOTH = PackageManager.FEATURE_BLUETOOTH
-    @RequiresApi(AndroidHelper.API_18)
     const val FEATURE_BLUETOOTH_LE = PackageManager.FEATURE_BLUETOOTH_LE
     const val FEATURE_CAMERA = PackageManager.FEATURE_CAMERA
-    @RequiresApi(AndroidHelper.API_17)
     const val FEATURE_CAMERA_ANY = PackageManager.FEATURE_CAMERA_ANY
     @RequiresApi(AndroidHelper.API_28)
     const val FEATURE_CAMERA_AR = PackageManager.FEATURE_CAMERA_AR
     const val FEATURE_CAMERA_AUTOFOCUS = PackageManager.FEATURE_CAMERA_AUTOFOCUS
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_CAMERA_CAPABILITY_MANUAL_POST_PROCESSING = PackageManager.FEATURE_CAMERA_CAPABILITY_MANUAL_POST_PROCESSING
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_CAMERA_CAPABILITY_MANUAL_SENSOR = PackageManager.FEATURE_CAMERA_CAPABILITY_MANUAL_SENSOR
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_CAMERA_CAPABILITY_RAW = PackageManager.FEATURE_CAMERA_CAPABILITY_RAW
-    @RequiresApi(AndroidHelper.API_20)
     const val FEATURE_CAMERA_EXTERNAL = PackageManager.FEATURE_CAMERA_EXTERNAL
     const val FEATURE_CAMERA_FLASH = PackageManager.FEATURE_CAMERA_FLASH
     const val FEATURE_CAMERA_FRONT = PackageManager.FEATURE_CAMERA_FRONT
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_CAMERA_LEVEL_FULL = PackageManager.FEATURE_CAMERA_LEVEL_FULL
     @RequiresApi(AndroidHelper.API_28)
     const val FEATURE_COMPANION_DEVICE_SETUP = PackageManager.FEATURE_COMPANION_DEVICE_SETUP
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_CONNECTION_SERVICE = PackageManager.FEATURE_CONNECTION_SERVICE
-    @RequiresApi(AndroidHelper.API_19)
     const val FEATURE_CONSUMER_IR = PackageManager.FEATURE_CONSUMER_IR
-    @RequiresApi(AndroidHelper.API_19)
     const val FEATURE_DEVICE_ADMIN = PackageManager.FEATURE_DEVICE_ADMIN
     @RequiresApi(AndroidHelper.API_26)
     const val FEATURE_EMBEDDED = PackageManager.FEATURE_EMBEDDED
@@ -60,36 +45,27 @@ object FeaturesHelper {
     const val FEATURE_FAKETOUCH = PackageManager.FEATURE_FAKETOUCH
     const val FEATURE_FAKETOUCH_MULTITOUCH_DISTINCT = PackageManager.FEATURE_FAKETOUCH_MULTITOUCH_DISTINCT
     const val FEATURE_FAKETOUCH_MULTITOUCH_JAZZHAND = PackageManager.FEATURE_FAKETOUCH_MULTITOUCH_JAZZHAND
-    @RequiresApi(AndroidHelper.API_23)
     const val FEATURE_FINGERPRINT = PackageManager.FEATURE_FINGERPRINT
     @RequiresApi(AndroidHelper.API_24)
     const val FEATURE_FREEFORM_WINDOW_MANAGEMENT = PackageManager.FEATURE_FREEFORM_WINDOW_MANAGEMENT
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_GAMEPAD = PackageManager.FEATURE_GAMEPAD
-    @RequiresApi(AndroidHelper.API_23)
     const val FEATURE_HIFI_SENSORS = PackageManager.FEATURE_HIFI_SENSORS
-    @RequiresApi(AndroidHelper.API_18)
     const val FEATURE_HOME_SCREEN = PackageManager.FEATURE_HOME_SCREEN
-    @RequiresApi(AndroidHelper.API_18)
     const val FEATURE_INPUT_METHODS = PackageManager.FEATURE_INPUT_METHODS
     @RequiresApi(AndroidHelper.API_29)
     const val FEATURE_IPSEC_TUNNELS = PackageManager.FEATURE_IPSEC_TUNNELS
     @RequiresApi(AndroidHelper.API_29)
     const val FEATURE_IRIS = PackageManager.FEATURE_IRIS
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_LEANBACK = PackageManager.FEATURE_LEANBACK
     @RequiresApi(AndroidHelper.API_26)
     const val FEATURE_LEANBACK_ONLY = PackageManager.FEATURE_LEANBACK_ONLY
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_LIVE_TV = PackageManager.FEATURE_LIVE_TV
     const val FEATURE_LIVE_WALLPAPER = PackageManager.FEATURE_LIVE_WALLPAPER
     const val FEATURE_LOCATION = PackageManager.FEATURE_LOCATION
     const val FEATURE_LOCATION_GPS = PackageManager.FEATURE_LOCATION_GPS
     const val FEATURE_LOCATION_NETWORK = PackageManager.FEATURE_LOCATION_NETWORK
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_MANAGED_USERS = PackageManager.FEATURE_MANAGED_USERS
     const val FEATURE_MICROPHONE = PackageManager.FEATURE_MICROPHONE
-    @RequiresApi(AndroidHelper.API_23)
     const val FEATURE_MIDI = PackageManager.FEATURE_MIDI
     const val FEATURE_NFC = PackageManager.FEATURE_NFC
     @RequiresApi(AndroidHelper.API_29)
@@ -102,13 +78,11 @@ object FeaturesHelper {
     const val FEATURE_NFC_OFF_HOST_CARD_EMULATION_ESE = PackageManager.FEATURE_NFC_OFF_HOST_CARD_EMULATION_ESE
     @RequiresApi(AndroidHelper.API_29)
     const val FEATURE_NFC_OFF_HOST_CARD_EMULATION_UICC = PackageManager.FEATURE_NFC_OFF_HOST_CARD_EMULATION_UICC
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_OPENGLES_EXTENSION_PACK = PackageManager.FEATURE_OPENGLES_EXTENSION_PACK
     @RequiresApi(AndroidHelper.API_27)
     const val FEATURE_PC = PackageManager.FEATURE_PC
     @RequiresApi(AndroidHelper.API_24)
     const val FEATURE_PICTURE_IN_PICTURE = PackageManager.FEATURE_PICTURE_IN_PICTURE
-    @RequiresApi(AndroidHelper.API_20)
     const val FEATURE_PRINTING = PackageManager.FEATURE_PRINTING
     @RequiresApi(AndroidHelper.API_27)
     const val FEATURE_RAM_LOW = PackageManager.FEATURE_RAM_LOW
@@ -116,27 +90,20 @@ object FeaturesHelper {
     const val FEATURE_RAM_NORMAL = PackageManager.FEATURE_RAM_NORMAL
     const val FEATURE_SCREEN_LANDSCAPE = PackageManager.FEATURE_SCREEN_LANDSCAPE
     const val FEATURE_SCREEN_PORTRAIT = PackageManager.FEATURE_SCREEN_PORTRAIT
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_SECURELY_REMOVES_USERS = PackageManager.FEATURE_SECURELY_REMOVES_USERS
     @RequiresApi(AndroidHelper.API_29)
     const val FEATURE_SECURE_LOCK_SCREEN = PackageManager.FEATURE_SECURE_LOCK_SCREEN
     const val FEATURE_SENSOR_ACCELEROMETER = PackageManager.FEATURE_SENSOR_ACCELEROMETER
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_SENSOR_AMBIENT_TEMPERATURE = PackageManager.FEATURE_SENSOR_AMBIENT_TEMPERATURE
     const val FEATURE_SENSOR_BAROMETER = PackageManager.FEATURE_SENSOR_BAROMETER
     const val FEATURE_SENSOR_COMPASS = PackageManager.FEATURE_SENSOR_COMPASS
     const val FEATURE_SENSOR_GYROSCOPE = PackageManager.FEATURE_SENSOR_GYROSCOPE
-    @RequiresApi(AndroidHelper.API_20)
     const val FEATURE_SENSOR_HEART_RATE = PackageManager.FEATURE_SENSOR_HEART_RATE
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_SENSOR_HEART_RATE_ECG = PackageManager.FEATURE_SENSOR_HEART_RATE_ECG
     const val FEATURE_SENSOR_LIGHT = PackageManager.FEATURE_SENSOR_LIGHT
     const val FEATURE_SENSOR_PROXIMITY = PackageManager.FEATURE_SENSOR_PROXIMITY
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_SENSOR_RELATIVE_HUMIDITY = PackageManager.FEATURE_SENSOR_RELATIVE_HUMIDITY
-    @RequiresApi(AndroidHelper.API_19)
     const val FEATURE_SENSOR_STEP_COUNTER = PackageManager.FEATURE_SENSOR_STEP_COUNTER
-    @RequiresApi(AndroidHelper.API_19)
     const val FEATURE_SENSOR_STEP_DETECTOR = PackageManager.FEATURE_SENSOR_STEP_DETECTOR
     const val FEATURE_SIP = PackageManager.FEATURE_SIP
     const val FEATURE_SIP_VOIP = PackageManager.FEATURE_SIP_VOIP
@@ -160,7 +127,6 @@ object FeaturesHelper {
     const val FEATURE_TOUCHSCREEN_MULTITOUCH_JAZZHAND = PackageManager.FEATURE_TOUCHSCREEN_MULTITOUCH_JAZZHAND
     const val FEATURE_USB_ACCESSORY = PackageManager.FEATURE_USB_ACCESSORY
     const val FEATURE_USB_HOST = PackageManager.FEATURE_USB_HOST
-    @RequiresApi(AndroidHelper.API_21)
     const val FEATURE_VERIFIED_BOOT = PackageManager.FEATURE_VERIFIED_BOOT
     @RequiresApi(AndroidHelper.API_26)
     const val FEATURE_VR_HEADTRACKING = PackageManager.FEATURE_VR_HEADTRACKING
@@ -175,9 +141,7 @@ object FeaturesHelper {
     const val FEATURE_VULKAN_HARDWARE_LEVEL = PackageManager.FEATURE_VULKAN_HARDWARE_LEVEL
     @RequiresApi(AndroidHelper.API_24)
     const val FEATURE_VULKAN_HARDWARE_VERSION = PackageManager.FEATURE_VULKAN_HARDWARE_VERSION
-    @RequiresApi(AndroidHelper.API_20)
     const val FEATURE_WATCH = PackageManager.FEATURE_WATCH
-    @RequiresApi(AndroidHelper.API_20)
     const val FEATURE_WEBVIEW = PackageManager.FEATURE_WEBVIEW
     const val FEATURE_WIFI = PackageManager.FEATURE_WIFI
     @RequiresApi(AndroidHelper.API_26)
--- core/src/main/kotlin/me/shkschneider/skeleton/helperx/SystemServices.kt
+++ core/src/main/kotlin/me/shkschneider/skeleton/helperx/SystemServices.kt
@@ -74,294 +74,79 @@ import kotlin.reflect.KClass
 // <http://developer.android.com/reference/android/content/Context.html>
 object SystemServices {
 
-    private fun get(service: String): Any? =
-            ContextHelper.applicationContext().getSystemService(service)
-
-
-    @RequiresApi(AndroidHelper.API_23)
-    private fun <T : Any> get(klass: KClass<T>): T? =
-            ContextHelper.applicationContext().getSystemService(klass)
-
-    fun accessibilityManager(): AccessibilityManager? =
-            get(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager?
-
-    fun accountManager(): AccountManager? =
-            get(Context.ACCOUNT_SERVICE) as AccountManager?
-
-
-    fun activityManager(): ActivityManager? =
-            get(Context.ACTIVITY_SERVICE) as ActivityManager?
-
-    fun alarmManager(): AlarmManager? =
-            get(Context.ALARM_SERVICE) as AlarmManager?
-
-    @RequiresApi(AndroidHelper.API_21)
-    fun appWidgetManager(): AppWidgetManager? =
-            get(Context.APPWIDGET_SERVICE) as AppWidgetManager?
-
-    @RequiresApi(AndroidHelper.API_19)
-    fun appOpsManager(): AppOpsManager? =
-            get(Context.APP_OPS_SERVICE) as AppOpsManager?
-
-    fun audioManager(): AudioManager? =
-            get(Context.AUDIO_SERVICE) as AudioManager?
-
-    @RequiresApi(AndroidHelper.API_21)
-    fun batteryManager(): BatteryManager? =
-            get(Context.BATTERY_SERVICE) as BatteryManager?
-
+    fun accessibilityManager() = get(AccessibilityManager::class)
+    fun accountManager() = get(AccountManager::class)
+    fun activityManager() = get(ActivityManager::class)
+    fun alarmManager() = get(AlarmManager::class)
+    fun appWidgetManager() = get(AppWidgetManager::class)
+    fun appOpsManager() = get(AppOpsManager::class)
+    fun audioManager() = get(AudioManager::class)
+    fun batteryManager() = get(BatteryManager::class)
     @RequiresApi(AndroidHelper.API_29)
-    fun biometricManager(): BiometricManager? =
-            get(Context.BIOMETRIC_SERVICE) as BiometricManager?
-
-    @RequiresApi(AndroidHelper.API_18)
-    fun bluetoothManager(): BluetoothManager? =
-            get(Context.BLUETOOTH_SERVICE) as BluetoothManager?
-
-
-    @RequiresApi(AndroidHelper.API_21)
-    fun cameraManager(): CameraManager? =
-            get(Context.CAMERA_SERVICE) as CameraManager?
-
-
-    @RequiresApi(AndroidHelper.API_19)
-    fun captioningManager(): CaptioningManager? =
-            get(Context.CAPTIONING_SERVICE) as CaptioningManager?
-
-
-    @RequiresApi(AndroidHelper.API_23)
-    fun carrierConfigManager(): CarrierConfigManager? =
-            get(Context.CARRIER_CONFIG_SERVICE) as CarrierConfigManager?
-
-
-    fun clipboardManager(): ClipboardManager? =
-            get(Context.CLIPBOARD_SERVICE) as ClipboardManager?
-
-
+    fun biometricManager() = get(BiometricManager::class)
+    fun bluetoothManager() = get(BluetoothManager::class)
+    fun cameraManager() = get(CameraManager::class)
+    fun captioningManager() = get(CaptioningManager::class)
+    fun carrierConfigManager() = get(CarrierConfigManager::class)
+    fun clipboardManager() = get(ClipboardManager::class)
     @RequiresApi(AndroidHelper.API_26)
-    fun companionDeviceManager(): CompanionDeviceManager? =
-            get(Context.COMPANION_DEVICE_SERVICE) as CompanionDeviceManager?
-
-
-    fun connectivityManager(): ConnectivityManager? =
-            get(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
-
-
-    @RequiresApi(AndroidHelper.API_19)
-    fun consumerIrManager(): ConsumerIrManager? =
-            get(Context.CONSUMER_IR_SERVICE) as ConsumerIrManager?
-
-
-    fun devicePolicyManager(): DevicePolicyManager? =
-            get(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager?
-
-
-    @RequiresApi(AndroidHelper.API_17)
-    fun displayManager(): DisplayManager? =
-            get(Context.DISPLAY_SERVICE) as DisplayManager?
-
-
-    fun downloadManager(): DownloadManager? =
-            get(Context.DOWNLOAD_SERVICE) as DownloadManager?
-
-
-    fun dropBoxManager(): DropBoxManager? =
-            get(Context.DROPBOX_SERVICE) as DropBoxManager?
-
-
-    fun fingerprintManager(context: Context): FingerprintManagerCompat =
-            FingerprintManagerCompat.from(context)
-
-
+    fun companionDeviceManager() = get(CompanionDeviceManager::class)
+    fun connectivityManager() = get(ConnectivityManager::class)
+    fun consumerIrManager() = get(ConsumerIrManager::class)
+    fun devicePolicyManager() = get(DevicePolicyManager::class)
+    fun displayManager() = get(DisplayManager::class)
+    fun downloadManager() = get(DownloadManager::class)
+    fun dropBoxManager() = get(DropBoxManager::class)
+    fun fingerprintManager(context: Context) = FingerprintManagerCompat.from(context)
     @RequiresApi(AndroidHelper.API_24)
-    fun hardwarePropertiesService(): HardwarePropertiesManager? =
-            get(Context.HARDWARE_PROPERTIES_SERVICE) as HardwarePropertiesManager?
-
-
-    fun inputMethodManager(): InputMethodManager? =
-            get(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
-
-
-    @RequiresApi(AndroidHelper.API_16)
-    fun inputManager(): InputManager? =
-            get(Context.INPUT_SERVICE) as InputManager?
-
-
-    @RequiresApi(AndroidHelper.API_21)
-    fun jobScheduler(): JobScheduler? =
-            get(Context.JOB_SCHEDULER_SERVICE) as JobScheduler?
-
-
-    fun keyguardManager(): KeyguardManager? =
-            get(Context.KEYGUARD_SERVICE) as KeyguardManager?
-
-
-    @RequiresApi(AndroidHelper.API_21)
-    fun launcherApps(): LauncherApps? =
-            get(Context.LAUNCHER_APPS_SERVICE) as LauncherApps?
-
-
-    fun layoutInflater(): LayoutInflater? =
-            get(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater?
-
-
-    fun locationManager(): LocationManager? =
-            get(Context.LOCATION_SERVICE) as LocationManager?
-
-
-    @RequiresApi(AndroidHelper.API_21)
-    fun mediaProjectionManager(): MediaProjectionManager? =
-            get(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager?
-
-
-    @RequiresApi(AndroidHelper.API_16)
-    fun mediaRouter(): MediaRouter? =
-            get(Context.MEDIA_ROUTER_SERVICE) as MediaRouter?
-
-
-    @RequiresApi(AndroidHelper.API_21)
-    fun mediaSessionManager(): MediaSessionManager? =
-            get(Context.MEDIA_SESSION_SERVICE) as MediaSessionManager?
-
-
-    @RequiresApi(AndroidHelper.API_23)
-    fun midiManager(): MidiManager? =
-            get(Context.MIDI_SERVICE) as MidiManager?
-
-
-    @RequiresApi(AndroidHelper.API_23)
-    fun networkStatsManager(): NetworkStatsManager? =
-            get(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager?
-
-
-    fun nfcManager(): NfcManager? =
-            get(Context.NFC_SERVICE) as NfcManager?
-
-
-    fun notificationManager(): NotificationManager? =
-            get(Context.NOTIFICATION_SERVICE) as NotificationManager?
-
-
-    @RequiresApi(AndroidHelper.API_16)
-    fun nsdManager(): NsdManager? =
-            get(Context.NSD_SERVICE) as NsdManager?
-
-
-    fun powerManager(): PowerManager? =
-            get(Context.POWER_SERVICE) as PowerManager?
-
-
-    @RequiresApi(AndroidHelper.API_19)
-    fun printManager(): PrintManager? =
-            get(Context.PRINT_SERVICE) as PrintManager?
-
-
-    @RequiresApi(AndroidHelper.API_21)
-    fun restrictionsManager(): RestrictionsManager? =
-            get(Context.RESTRICTIONS_SERVICE) as RestrictionsManager?
-
-
+    fun hardwarePropertiesService() = get(HardwarePropertiesManager::class)
+    fun inputMethodManager() = get(InputMethodManager::class)
+    fun inputManager() = get(InputManager::class)
+    fun jobScheduler() = get(JobScheduler::class)
+    fun keyguardManager() = get(KeyguardManager::class)
+    fun launcherApps() = get(LauncherApps::class)
+    fun layoutInflater() = get(LayoutInflater::class)
+    fun locationManager() = get(LocationManager::class)
+    fun mediaProjectionManager() = get(MediaProjectionManager::class)
+    fun mediaRouter() = get(MediaRouter::class)
+    fun mediaSessionManager() = get(MediaSessionManager::class)
+    fun midiManager() = get(MidiManager::class)
+    fun networkStatsManager() = get(NetworkStatsManager::class)
+    fun nfcManager() = get(NfcManager::class)
+    fun notificationManager() = get(NotificationManager::class)
+    fun nsdManager() = get(NsdManager::class)
+    fun powerManager() = get(PowerManager::class)
+    fun printManager() = get(PrintManager::class)
+    fun restrictionsManager() = get(RestrictionsManager::class)
     @RequiresApi(AndroidHelper.API_29)
-    fun roleManager(): RoleManager? =
-            get(Context.ROLE_SERVICE) as RoleManager?
-
-
-    fun searchManager(): SearchManager? =
-            get(Context.SEARCH_SERVICE) as SearchManager?
-
-
-    fun sensorManager(): SensorManager? =
-            get(Context.SENSOR_SERVICE) as SensorManager?
-
-
+    fun roleManager() = get(RoleManager::class)
+    fun searchManager() = get(SearchManager::class)
+    fun sensorManager() = get(SensorManager::class)
     @RequiresApi(AndroidHelper.API_25)
-    fun shortcutManager(): ShortcutManager? =
-            get(Context.SHORTCUT_SERVICE) as ShortcutManager?
-
-
-    fun storageManager(): StorageManager? =
-            get(Context.STORAGE_SERVICE) as StorageManager?
-
-
+    fun shortcutManager() = get(ShortcutManager::class)
+    fun storageManager() = get(StorageManager::class)
     @RequiresApi(AndroidHelper.API_26)
-    fun storageStatsManager(): StorageStatsManager? =
-            get(Context.STORAGE_STATS_SERVICE) as StorageStatsManager?
-
-
+    fun storageStatsManager() = get(StorageStatsManager::class)
     @RequiresApi(AndroidHelper.API_24)
-    fun systemHealthService(): SystemHealthManager? =
-            get(Context.SYSTEM_HEALTH_SERVICE) as SystemHealthManager?
-
-
-    @RequiresApi(AndroidHelper.API_21)
-    fun telecomManager(): TelecomManager? =
-            get(Context.TELECOM_SERVICE) as TelecomManager?
-
-
-    fun telephonyManager(): TelephonyManager? =
-            get(Context.TELEPHONY_SERVICE) as TelephonyManager?
-
-
-    @RequiresApi(AndroidHelper.API_22)
-    fun subscriptionManager(): SubscriptionManager? =
-            get(Context.TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager?
-
-
+    fun systemHealthService() = get(SystemHealthManager::class)
+    fun telecomManager() = get(TelecomManager::class)
+    fun telephonyManager() = get(TelephonyManager::class)
+    fun subscriptionManager() = get(SubscriptionManager::class)
+    @RequiresApi(AndroidHelper.API_26) fun textClassificationManager() = get(TextClassificationManager::class)
+    fun textServicesManager() = get(TextServicesManager::class)
+    fun tvInputManager() = get(TvInputManager::class)
+    fun uiModeManager() = get(UiModeManager::class)
+    fun usageStatsManager() = get(UsageStatsManager::class)
+    fun usbManager() = get(UsbManager::class)
+    fun userManager() = get(UserManager::class)
+    fun vibrator() = get(Vibrator::class)
+    fun wallpaperService() = get(WallpaperService::class)
     @RequiresApi(AndroidHelper.API_26)
-    fun textClassificationManager(): TextClassificationManager? =
-            get(Context.TEXT_CLASSIFICATION_SERVICE) as TextClassificationManager?
+    fun wifiAwareManager() = get(WifiAwareManager::class)
+    fun wifiP2pManager(): WifiP2pManager? = get(WifiP2pManager::class)
+    fun wifiManager() = get(WifiManager::class)
+    fun windowManager() = get(WindowManager::class)
 
-
-    fun textServicesManager(): TextServicesManager? =
-            get(Context.TEXT_SERVICES_MANAGER_SERVICE) as TextServicesManager?
-
-
-    @RequiresApi(AndroidHelper.API_21)
-    fun tvInputManager(): TvInputManager? =
-            get(Context.TV_INPUT_SERVICE) as TvInputManager?
-
-
-    fun uiModeManager(): UiModeManager? =
-            get(Context.UI_MODE_SERVICE) as UiModeManager?
-
-
-    @RequiresApi(AndroidHelper.API_22)
-    fun usageStatsManager(): UsageStatsManager? =
-            get(Context.USAGE_STATS_SERVICE) as UsageStatsManager?
-
-
-    fun usbManager(): UsbManager? =
-            get(Context.USB_SERVICE) as UsbManager?
-
-
-    @RequiresApi(AndroidHelper.API_17)
-    fun userManager(): UserManager? =
-            get(Context.USER_SERVICE) as UserManager?
-
-
-    fun vibrator(): Vibrator? =
-            get(Context.VIBRATOR_SERVICE) as Vibrator?
-
-
-    fun wallpaperService(): WallpaperService? =
-            get(Context.WALLPAPER_SERVICE) as WallpaperService?
-
-
-    @RequiresApi(AndroidHelper.API_26)
-    fun wifiAwareManager(): WifiAwareManager? =
-            get(Context.WIFI_AWARE_SERVICE) as WifiAwareManager?
-
-
-    fun wifiP2pManager(): WifiP2pManager? =
-            get(Context.WIFI_P2P_SERVICE) as WifiP2pManager?
-
-
-    fun wifiManager(): WifiManager? =
-            get(Context.WIFI_SERVICE) as WifiManager?
-
-
-    fun windowManager(): WindowManager? =
-            get(Context.WINDOW_SERVICE) as WindowManager?
-
+    private fun <T : Any> get(klass: KClass<T>) = ContextHelper.applicationContext().getSystemService(klass)
 
 }
