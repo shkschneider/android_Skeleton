c451dbdb28167eee4151def80cf53ddbcba1d3cc
alan.schneider <alan.schneider@adevinta.com>
2020-08-24 13:25:16 +0200

    Gradle tasks rework

--- kotlin/src/main/kotlin/me/shkschneider/skeleton/kotlin/crypt/KeyStore.kt
+++
@@ -1,9 +0,0 @@
-package me.shkschneider.skeleton.kotlin.crypt
-
-import java.security.PrivateKey
-import java.security.PublicKey
-
-data class KeyStore(
-    val privateKey: PrivateKey,
-    val publicKey: PublicKey
-)
