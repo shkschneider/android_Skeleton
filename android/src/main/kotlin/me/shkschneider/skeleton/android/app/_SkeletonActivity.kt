a8b0a4e22cdf4bb2cb1b920a9c83f297d1e6e70e
alan.schneider <alan.schneider@adevinta.com>
2020-08-25 16:36:54 +0200

    SkeletonActivity: ViewModel

--- android/src/main/kotlin/me/shkschneider/skeleton/android/app/_SkeletonActivity.kt
+++ android/src/main/kotlin/me/shkschneider/skeleton/android/app/_SkeletonActivity.kt
@@ -1,13 +1,14 @@
 package me.shkschneider.skeleton.android.app
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 fun SkeletonActivity.getViewModelProviders(): ViewModelProvider =
     ViewModelProvider(this)
 
-//inline fun <reified T : ViewModel> SkeletonActivity.getViewModel(): T =
-//    getViewModelProviders().get(T::class.java)
-// FIXME
-//inline fun <reified T : ViewModel> SkeletonActivity.viewModel() = lazy {
-//    getViewModelProviders().get(T::class.java)
-//}
+inline fun <reified T : ViewModel> SkeletonActivity.getViewModel(): T =
+    getViewModelProviders().get(T::class.java)
+
+inline fun <reified T : ViewModel> SkeletonActivity.viewModel() = lazy {
+    getViewModelProviders().get(T::class.java)
+}
