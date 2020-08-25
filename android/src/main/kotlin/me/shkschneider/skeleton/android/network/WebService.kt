9b16b04b46711bc5e978c4e472ca0ec1deb3ef66
alan.schneider <alan.schneider@adevinta.com>
2020-08-25 16:36:07 +0200

    Fuel2 with reworked WebService

--- android/src/main/kotlin/me/shkschneider/skeleton/android/network/WebService.kt
+++ android/src/main/kotlin/me/shkschneider/skeleton/android/network/WebService.kt
@@ -2,17 +2,42 @@ package me.shkschneider.skeleton.android.network
 
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.github.kittinunf.result.Result
 import com.google.gson.Gson
+import me.shkschneider.skeleton.android.app.ApplicationHelper
+import me.shkschneider.skeleton.kotlin.jvm.tryOrNull
 import me.shkschneider.skeleton.kotlin.log.Logger
+import java.util.concurrent.TimeUnit
+import kotlin.reflect.KClass
 
 typealias WebServiceSuccess<T> = (Request, Response, T?) -> Unit
-typealias WebServiceFailure = (Request, Response, Exception) -> Unit
+typealias WebServiceFailure = (Request, Response, Throwable) -> Unit
 
 @Suppress("DEPRECATION")
-open class WebService(gson: Gson = Gson()) : BaseWebService(gson) {
+open class WebService(val gson: Gson = Gson()) {
+
+    init {
+        if (ApplicationHelper.debuggable) {
+            FuelManager.instance.addRequestInterceptor { next: (Request) -> Request ->
+                { request: Request ->
+                    Logger.info(request.toString())
+                    next(request)
+                }
+            }
+            FuelManager.instance.addResponseInterceptor { next: (Request, Response) -> Response ->
+                { request: Request, response: Response ->
+                    Logger.info(response.toString())
+                    next(request, response)
+                }
+            }
+        }
+        FuelManager.instance.timeoutInMillisecond = TimeUnit.SECONDS.toMillis(15).toInt()
+        FuelManager.instance.timeoutReadInMillisecond = TimeUnit.SECONDS.toMillis(15).toInt()
+    }
 
     inline fun <reified T : Any> get(url: String, crossinline success: WebServiceSuccess<T>, noinline failure: WebServiceFailure? = null): Request =
         request<T>(Fuel.get(url), success, failure)
@@ -39,5 +64,14 @@ open class WebService(gson: Gson = Gson()) : BaseWebService(gson) {
                 failure?.invoke(request, response, fuelError.exception)
             })
         }
+
+    class Deserializer<T : Any>(private val gson: Gson, private val klass: KClass<T>) : ResponseDeserializable<T> {
+
+        override fun deserialize(content: String): T? =
+            tryOrNull {
+                gson.fromJson(content, klass.java)
+            }
+
+    }
 
 }
